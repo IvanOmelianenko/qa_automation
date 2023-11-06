27986c22264cc4cd9726f95f93bbf7bc26036b10
Ivan <vanyokredray@gmail.com>
2023-11-06 14:34:51 +0200

    added homework 6

---
+++ lesson_6_hw.js
@@ -0,0 +1,68 @@
+/*
+1. Написати функцію, яка перетворює строку на масив слів,
+    сортує його за алфавітним порядком і повертає (у масиві не повинно бути пробілів як елементів)
+ */
+
+const str = "Це довільний текст для створення масиву слів ";
+function stringToArray(str) {
+    // Ваш код
+    let arr = str.trim()
+    .split(/\s+/)
+    .map(word => word.toLowerCase())
+    .sort()
+return arr 
+}
+
+
+console.log(stringToArray(str));    // виводить [ 'для', 'довільний', 'масиву', 'слів', 'створення', 'текст', 'це' ]
+
+/*
+2. Написати функцію, яка видаляє з масива елементи, що дублюються, та повертає масив оригінальних елементів,
+відсортованих за зростанням
+ */
+const initialArray = [1, 4, 6, 6, 7, 5, 34, 5, 1000, 66, 1];
+function removeDuplicatesAndSort(arr) {
+    // Ваш код
+    let newSet = new Set(arr)
+    let sortingValue = Array.from(newSet).sort((a, b) => (a - b))
+    return sortingValue
+}
+
+console.log(removeDuplicatesAndSort(initialArray)); // виводить [ 1, 4, 5, 6, 7, 34, 66, 1000 ]
+/*
+3. Написати функцію, яка поверне масив парних чисел
+ */
+const initialArray2 = [2, 3, 6, 7, 9, 12];
+function arrayOfEvens(arr) {
+    // Ваш код
+    let onlyEvens = []
+    for(number of arr) {
+        if(number % 2 === 0){
+        onlyEvens.push(number)
+    }
+    }
+    return onlyEvens
+
+}
+
+console.log(arrayOfEvens(initialArray2)); // виводить [ 2, 6, 12 ]
+
+/*
+4. Написати функцію, яка сформує масив з послідовності чисел, формуючи його з аргумента, який збільшує
+кожний наступний елемент послідовності на самого себе. Максимальне значення масиву не повинно перевищувати число 30.
+ */
+function createArray(element) {
+    // Ваш код
+    let mulArr = []
+    let numFromArr = element
+
+    while(numFromArr <= 30) {
+        mulArr.push(numFromArr)
+        numFromArr += element
+    }
+    return mulArr
+}
+
+console.log(createArray(5)); // виведе [ 5, 10, 15, 20, 25, 30 ]
+console.log(createArray(3)); // виведе [ 3,  6,  9, 12, 15, 18, 21, 24, 27, 30 ]
+
